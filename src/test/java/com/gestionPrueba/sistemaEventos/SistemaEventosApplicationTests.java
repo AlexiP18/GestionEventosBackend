d96c6b3e51847da658e09945a77b426ce815f573
Alexis Poaquiza <apoaquiza6287@uta.edu.ec>
2024-04-23 21:08:28 -0500

    Subida de proyecto con cambios en el system.properties

---
+++ src/test/java/com/gestionPrueba/sistemaEventos/SistemaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionPrueba.sistemaEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
